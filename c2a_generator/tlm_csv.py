5cec3b7a95a4ebcc54801e99b5fd6a0632101d34
flap1 <sho1seto@gmail.com>
2024-03-24 12:41:15 +0900

    feat: add original tlm generator

--- c2a_generator/tlm_csv.py
+++ c2a_generator/tlm_csv.py
@@ -13,10 +13,11 @@ def generate(src_dir_path: Path, dest_dir_path: Path, prefix: str) -> None:
     for src_path in src_path_list:
         dest_name = prefix + os.path.basename(src_path)
         dest_path = dest_dir_path / dest_name
-        generate_(src_path, dest_path)
+        dest_calced_data_path = dest_dir_path / "calced_data" / dest_name
+        generate_(src_path, dest_path, dest_calced_data_path)
 
 
-def generate_(src_file_path: Path, dest_file_path: Path) -> None:
+def generate_(src_file_path: Path, dest_file_path: Path, dest_calced_data_path: Path) -> None:
     dest_line_len = 18
     dest_line_max = 500
     line_index = 8
@@ -24,63 +25,81 @@ def generate_(src_file_path: Path, dest_file_path: Path) -> None:
     pos = 0
     bit_len = 0
 
-    with open(src_file_path, "r", encoding="utf-8") as src_file, open(dest_file_path, "w", encoding="utf-8") as dest_file:
+    with open(src_file_path, "r", encoding="utf-8") as src_file, open(dest_calced_data_path, "w", encoding="utf-8") as dest_calced_data, open(dest_file_path, "w", encoding="utf-8") as dest_file:
         reader = csv.reader(src_file)
-        reader = list(reader)
 
-        row0 = reader[0]
-        row0 = [row0[i].replace("\n", "##") for i in range(len(row0))]
-        row0 = [row0[i].replace(",", "@@") for i in range(len(row0))]
+        meta = next(reader)
+        headers = next(reader)
+        dict_reader = csv.DictReader(src_file, fieldnames=headers)
 
-        dest_file.write(f",Target,{row0[2]},Local Var")
-        dest_file.write("," * (dest_line_len - 4) + "\n")
-        dest_file.write(f",PacketID,0x{int(row0[1]):02x},{row0[3]}")
-        dest_file.write("," * (dest_line_len - 4) + "\n")
-        dest_file.write(",Enable/Disable,ENABLE")
-        dest_file.write("," * (dest_line_len - 3) + "\n")
-        dest_file.write(f",IsRestricted,{row0[0]}")
-        dest_file.write("," * (dest_line_len - 3) + "\n")
-        dest_file.write("," * (dest_line_len - 1) + "\n")
-        dest_file.write(
-            """
+        for i in range(len(meta)):
+            meta[i] = meta[i].replace("\n", "##").replace(",", "@@")
+
+        dest_calced_data.write(f"""
+,Target,{meta[2]},Local Var{"," * (dest_line_len - 4)}
+,PacketID,0x{int(meta[1]):02x},{meta[3]}{"," * (dest_line_len - 4)}
+,Enable/Disable,ENABLE{"," * (dest_line_len - 3)}
+,IsRestricted,{meta[0]}{"," * (dest_line_len - 3)}
+{"," * (dest_line_len - 1)}
 Comment,TLM Entry,Onboard Software Info.,,Extraction Info.,,,,Conversion Info.,,,,,,,,Description,Note
 ,Name,Var.%%##Type,Variable or Function Name,Ext.%%##Type,Pos. Desiginator,,,Conv.%%##Type,Poly (Σa_i * x^i),,,,,,Status,,
 ,,,,,Octet%%##Pos.,bit%%##Pos.,bit%%##Len.,,a0,a1,a2,a3,a4,a5,,,
-"""[
-                1:
-            ]
-        )
+"""[1:])
+        dest_file.write(f"""
+,Target,{meta[2]},Local Var{"," * (dest_line_len - 4)}
+,PacketID,0x{int(meta[1]):02x},{meta[3]}{"," * (dest_line_len - 4)}
+,Enable/Disable,ENABLE{"," * (dest_line_len - 3)}
+,IsRestricted,{meta[0]}{"," * (dest_line_len - 3)}
+{"," * (dest_line_len - 1)}
+Comment,TLM Entry,Onboard Software Info.,,Extraction Info.,,,,Conversion Info.,,,,,,,,Description,Note
+,Name,Var.%%##Type,Variable or Function Name,Ext.%%##Type,Pos. Desiginator,,,Conv.%%##Type,Poly (Σa_i * x^i),,,,,,Status,,
+,,,,,Octet%%##Pos.,bit%%##Pos.,bit%%##Len.,,a0,a1,a2,a3,a4,a5,,,
+"""[1:])
 
-        reader = reader[2:]
-
-        for row in reader:
-            row = row[1:]
-            row = [row[i].replace("\n", "##") for i in range(len(row))]
-            row = [row[i].replace(",", "@@") for i in range(len(row))]
+        for row in dict_reader:
+            for key in ["var", "status", "description", "note"]:
+                row[key] = row[key].replace("\n", "##").replace(",", "@@")
             if not any(row):
                 line_index += 1
                 continue
-            if row[1]:
-                if row[1][0] == "_":
-                    row[1] = row[1][1:]
-                    bit_len = int(row[2])
+
+            type_original = row["type"]
+            if row["type"]:
+                if row["type"][0] == "_":
+                    type_original = row["type"][1:]
+                    row["type"] = row["type"][1:]
+                    bit_len = int(row["bit"])
                 else:
-                    bit_len = type2bit(row[1])
+                    bit_len = type2bit(row["type"])
             else:
-                bit_len = int(row[2])
+                bit_len = int(row["bit"])
+                type_original = "||"
 
             bit_pos = pos % 8
             octet_pos = int(pos / 8)
             pos += bit_len
 
-            if row[0][-5:] == "DUMMY" or row[0][0:5] == "DUMMY":
+            if row["name"][-5:] == "DUMMY" or row["name"][0:5] == "DUMMY":
+                dest_calced_data.write("*")
                 dest_file.write("*")
 
-            dest_file.write(
-                f",{row[0]},{row[1]},{row[3]},PACKET,{octet_pos},{bit_pos},{bit_len},{row[4]},{row[5]},{row[6]},{row[7]},{row[8]},\
-{row[9]},{row[10]},{row[11]},{row[12]},{row[13]}\n"
+            dest_calced_data.write(
+                f",{row['name']},{row['type']},{row['var']},PACKET,{octet_pos},{bit_pos},{bit_len},{row['conv']},{row['a0']},{row['a1']},{row['a2']},{row['a3']},\
+{row['a4']},{row['a5']},{row['status']},{row['description']},{row['note']}\n"
             )
+
+            octet_pos_original = "=R[-1]C+INT((R[-1]C[1]+R[-1]C[2])/8)" if row["name"] != "PH.VER" else "0"
+            bit_pos_original = "=MOD((R[-1]C+R[-1]C[1])@@8)" if row["name"] != "PH.VER" else "0"
+            bit_len_original = '=IF(OR(EXACT(RC[-5]@@"uint8_t")@@EXACT(RC[-5]@@"int8_t"))@@8@@IF(OR(EXACT(RC[-5]@@"uint16_t")@@EXACT(RC[-5]@@"int16_t"))@@16@@IF(OR(EXACT(RC[-5]@@"uint32_t")@@EXACT(RC[-5]@@"int32_t")@@EXACT(RC[-5]@@"float"))@@32@@IF(EXACT(RC[-5]@@"double")@@64))))' if not row["bit"] else row["bit"]
+            var_original = row["var"]
+            if not row["var"] and (len(row["name"]) > 3 and row["name"][0:3] not in ["PH.", "SH."]):
+                var_original = "||"
+            dest_file.write(
+                f',{row["name"]},{type_original},{var_original},PACKET,{octet_pos_original},{bit_pos_original},{bit_len_original},{row["conv"]},{row["a0"]},{row["a1"]},{row["a2"]},{row["a3"]},{row["a4"]},{row["a5"]},{row["status"]},{row["description"]},{row["note"]}\n'
+            )
+
             line_index += 1
+        dest_calced_data.write(("," * (dest_line_len - 1) + "\n") * (dest_line_max - line_index))
         dest_file.write(("," * (dest_line_len - 1) + "\n") * (dest_line_max - line_index))
 
 
