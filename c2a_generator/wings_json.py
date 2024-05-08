de338acb2cfa1554c3f7c8f4c8dd10e989497cdc
flap1 <sho1seto@gmail.com>
2024-05-09 06:13:43 +0900

    feat: wings json abse

--- c2a_generator/wings_json.py
+++ c2a_generator/wings_json.py
@@ -1,10 +1,16 @@
 import csv
 import json
 from pathlib import Path
+from typing import Optional
 
 
-def generate(bct_src: list, dest_path: Path) -> None:
-    data = [{"obc_name": "MOBC", "bc": [], "el": [], "eh": []}]
+def generate(bct_src: list, dest_path: Path, subobc_json_path: Optional[Path] = None) -> None:
+    if subobc_json_path is not None:
+        with open(subobc_json_path, "r", encoding="utf-8") as subobc_file:
+            data = json.load(subobc_file)
+    else:
+        data = []
+    data.append({"obc_name": "MOBC", "bc": [], "el": [], "eh": []})
     bcid = 0
     for src_path, bcid_base in bct_src:
         if bcid_base is not None:
@@ -20,7 +26,7 @@ def generate(bct_src: list, dest_path: Path) -> None:
                     continue
                 if not row["name"].strip():
                     continue
-                data[0]["bc"].append({"name": row["name"], "id": bcid})
+                data[-1]["bc"].append({"name": row["name"], "id": bcid})
                 bcid += 1
 
     with open(dest_path, "w", encoding="utf-8") as dest_file:
